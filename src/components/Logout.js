9c8ab2d64d9ed99169c3e80fa10d06b9dad6f2fd
Tibz-Dankan <tibesigwadankan@gmail.com>
2021-12-09 19:21:59 +0300

    the link hook removed from import of react-router-dom

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useHistory, Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import NotLoggedIn from "./NotLoggedIn";
 import "./../css/Logout.css";
 
