ec62caf484274ee74580220442174bf8bf5ab0b5
Tibz-Dankan <tibesigwadankan@gmail.com>
2021-12-27 02:05:11 +0300

    timer half way implement and VerifyEmail.js component styled

--- src/components/VerifyEmail.js
+++ src/components/VerifyEmail.js
@@ -3,6 +3,7 @@ import { useHistory, useLocation } from "react-router-dom";
 import "./../css/VerifyEmail.css";
 import axios from "axios";
 import { FadeLoader } from "react-spinners";
+import Footer from "./Footer";
 
 const VerifyEmail = () => {
   const location = useLocation();
@@ -17,71 +18,93 @@ const VerifyEmail = () => {
     useState(false);
   const [successMsg, setSuccessMsg] = useState("");
   const [errorMsg, setErrorMsg] = useState("");
-  const [count, setCount] = useState(5);
+  const [count, setCount] = useState(20);
+  const [showCaughtError, setShowCaughtError] = useState(false);
+  const [showTimer, setShowTimer] = useState(false);
 
-  const redirectToLoginAfterSeconds = () => {
+  const redirectToLoginAfterFiveSeconds = () => {
     setTimeout(() => {
       history.push("/login");
     }, 5000);
   };
 
-  const increaseCounterEverySecond = () => {
-    setInterval(() => {
+  const decreaseCounterEverySecond = () => {
+    return setInterval(() => {
       setCount(count - 1);
     }, 1000);
   };
 
   const verifyCode = async () => {
     try {
-      if (userId == null || verificationCode == null) {
-        setErrorMsg("Invalid link");
-      } else {
-        setShowFadeLoader(true);
-        const response = await axios.post(
-          ` http://localhost:5000/verify-user-email/${parseInt(userId)}`,
-          {
-            verificationCode: parseInt(verificationCode),
-          }
-        );
-        setSuccessMsg(response.data.verificationStatusMsg);
-        console.log(response); //to be commented out
-        if (response.status === 200) {
-          setShowFadeLoader(false);
-          setShowSuccessVerificationMsg(true);
-          redirectToLoginAfterSeconds();
-        } else {
-          //   an error occurred during email verification
+      if (userId == null || verificationCode == null)
+        return setErrorMsg("Invalid link !");
+      setShowCaughtError(false);
+      setShowFadeLoader(true);
+      const response = await axios.post(
+        ` http://localhost:5000/verify-user-email/${parseInt(userId)}`,
+        {
+          verificationCode: parseInt(verificationCode),
         }
+      );
+      setSuccessMsg(response.data.verificationStatusMsg);
+      console.log(response); //to be commented out
+      if (response.status === 200) {
+        setShowFadeLoader(false);
+        setShowTimer(true);
+        setShowSuccessVerificationMsg(true);
+        redirectToLoginAfterFiveSeconds();
+      } else {
+        //   an error occurred during email verification
       }
     } catch (error) {
       console.log(error);
       setShowFadeLoader(false);
+      setShowCaughtError(true);
+      setShowTimer(true);
+      // provide with a button to reload in case of a server error
     }
   };
 
   useEffect(() => {
     verifyCode();
+    // decreaseCounterEverySecond();
   }, []);
+
+  useEffect(() => {
+    // if (showTimer == true) {
+    const counter =
+      count > 0 &&
+      // setShowTimer(true) &&
+      setInterval(() => {
+        setCount(count - 1);
+      }, 1000);
+    return () => clearInterval(counter);
+    // }
+  }, [count]);
 
   return (
     <div className="verify-email-wrapper">
-      <p style={{ color: "red" }}>{errorMsg}</p>
-
-      {/* react spinner */}
+      <header className="verify-email-header">
+        <h1>Email Verification</h1>
+      </header>
+      {showCaughtError ? (
+        <p style={{ color: "red" }}>Sorry, something went wrong!</p>
+      ) : null}
+      <p style={{ color: "red", textAlign: "center" }}>{errorMsg}</p>
       {showFadeLoader ? (
         <div className="fade-loader">
+          <h4>verifying your email...</h4>
           <FadeLoader color="lightseagreen" />
-          <h5>verifying your account</h5>
         </div>
       ) : null}
-      {showSuccessVerificationMsg ? (
-        <div>
-          <p>{successMsg}</p>
-          <p onLoad={increaseCounterEverySecond}>
-            Redirects to login in {count}s
-          </p>
-        </div>
-      ) : null}
+      {/* {showSuccessVerificationMsg ? ( */}
+      <p>{successMsg}</p>
+      {/* <p onLoad={decreaseCounterEverySecond}>Redirects to login in {count}s</p> */}
+      <p>Redirects to login in {count}s</p>
+      {/* ) : null} */}
+      <div className="verify-email-footer">
+        <Footer />
+      </div>
     </div>
   );
 };
