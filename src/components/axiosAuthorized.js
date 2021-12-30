607e09d06ce820538b274efbe657f72db4e9aa37
Tibz-Dankan <tibesigwadankan@gmail.com>
2021-12-30 03:01:36 +0300

    url changed to production one

--- src/components/axiosAuthorized.js
+++ src/components/axiosAuthorized.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosApiAuthorized = axios.create({
-  baseURL: "http://localhost:5000",
-  //   baseURL: "https://stockpile-backend.herokuapp.com",
+  // baseURL: "http://localhost:5000",
+  baseURL: "https://stockpile-backend.herokuapp.com",
   headers: {
     Authorization: "Bearer " + localStorage.getItem("accessToken"),
   },
