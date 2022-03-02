7689e84dbbaf30fcdae87b60cdad35b7597a51d5
Tibz-Dankan <tibesigwadankan@gmail.com>
2022-03-02 11:55:08 +0300

    icons added to the header links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,11 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import {
+  FileTextFill,
+  InfoCircle,
+  BoxArrowInRight,
+  PencilSquare,
+} from "react-bootstrap-icons";
 import "./../App.css";
 
 const Header = () => {
@@ -7,16 +13,20 @@ const Header = () => {
     <div>
       <nav>
         <header className="header-nav-links">
-          <NavLink to="/todos" className="header-link">
+          <NavLink to="/todos" className="header-link header-link-icon">
+            <FileTextFill style={{ marginRight: "3px" }} />
             Todos
           </NavLink>
-          <NavLink to="/about" className="header-link">
+          <NavLink to="/about" className="header-link header-link-icon">
+            <InfoCircle style={{ marginRight: "3px" }} />
             About
           </NavLink>
-          <NavLink to="/login" className="header-link">
+          <NavLink to="/login" className="header-link header-link-icon">
+            <BoxArrowInRight style={{ marginRight: "3px" }} size={18.5} />
             Login
           </NavLink>
-          <NavLink to="/signup" className="header-link">
+          <NavLink to="/signup" className="header-link header-link-icon">
+            <PencilSquare style={{ marginRight: "3px" }} />
             SignUp
           </NavLink>
         </header>
