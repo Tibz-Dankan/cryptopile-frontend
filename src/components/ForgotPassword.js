071d0c89dd836eeed0d753c2b822ee8aea0726d1
Tibz-Dankan <tibesigwadankan@gmail.com>
2022-03-08 18:47:13 +0300

    a link to the home page added in the header

--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { BarLoader } from "react-spinners";
+import HomeLink from "./links/HomeLink";
+import { HouseFill } from "react-bootstrap-icons";
 import axiosApiUnAuthorized from "./axiosUnAuthorized";
 import "./../css/ForgotPassword.css";
 
@@ -18,17 +20,20 @@ const ForgotPassword = () => {
     try {
       setShowBarLoader(false);
       setShowBarLoader(true);
+      setShowCaughtError(false);
       setErrorInSendingPasswordResetCode("");
       const response = await axiosApiUnAuthorized.post("/forgot-password", {
         userEmail: userEmail,
       });
       console.log(response); // to be removed wen in production
       setShowBarLoader(false); // stop BarLoader
-      if (response.data.email === userEmail) {
-        localStorage.setItem("userEmail", response.data.email);
-        localStorage.setItem("userId", response.data.id);
+      if (response.status === 200) {
         setErrorInSendingPasswordResetCode(response.data.forgotPasswordMsg);
-        history.push("/password-reset-code");
+        if (response.data.email === userEmail) {
+          localStorage.setItem("userEmail", response.data.email);
+          localStorage.setItem("userId", response.data.userid);
+          history.push("/password-reset-code");
+        }
       }
     } catch (error) {
       console.log(error);
@@ -44,14 +49,17 @@ const ForgotPassword = () => {
 
   return (
     <div className="forgot-password-wrapper">
-      <div className="forgot-password-heading">
-        <h2>Forgot Password</h2>
-      </div>
-      {showCaughtError ? (
-        <div className="caught-error">
-          <p>Sorry, something went wrong !</p>
+      <div className="forgot-password-header">
+        <div className="forgot-password-home-link">
+          <HouseFill
+            color={"hsl(0, 0%, 100%)"}
+            size={17}
+            style={{ marginRight: "3px" }}
+          />
+          <HomeLink />
         </div>
-      ) : null}
+        <h4>Forgot Password</h4>
+      </div>
       <div className="forgot-password-msg">
         <p>
           Incase you forget your password or you just want to reset it, we
@@ -60,6 +68,12 @@ const ForgotPassword = () => {
         </p>
       </div>
       {showBarLoader ? <BarLoader color="hsl(180, 40%, 50%)" /> : null}
+
+      {showCaughtError ? (
+        <div className="caught-error">
+          <p>Sorry, something went wrong!</p>
+        </div>
+      ) : null}
       <div className="errors-in-sending-pwsd-reset-code">
         <p>{errorInSendingPasswordResetCode}</p> {/* to be styled next day*/}
       </div>
