9257721a5eec1a2581c37cad967c4f40dd1e8426
Tibz-Dankan <tibesigwadankan@gmail.com>
2022-03-13 18:10:46 +0300

    Footer component added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,42 @@
 import React from "react";
-import "./../App.css";
+import "./../css/Footer.css";
+import { Linkedin, Github, Twitter } from "react-bootstrap-icons";
 
 const Footer = () => {
   return (
     <div className="footer-wrapper">
-      <p>This site was founded, is run and managed by Tibz Dankan </p>
-      {/* the founder section to be added here */}
-      <p>Founder</p>
-      {/* the contact feature to be added here */}
-      <p>Contact</p>
+      <footer className="footer-content">
+        <p className="copyright-msg">
+          Copyright &copy; 2022 CryptoPile. All rights reserved
+        </p>
+        <p className="cryptopile-founder-manager">
+          CryptoPile is managed by Tibesigwa Dankan aka Tibz Dankan
+        </p>
+        <p className="follow-msg">Follow Dankan on</p>
+        <div className="footer-links-wrapper">
+          <nav className="footer-link-with-icon">
+            <Linkedin />
+            <a
+              href="https://www.linkedin.com/in/tibz-dankan-74158721b/?originalSubdomain=ug"
+              className="footer-link"
+            >
+              LinkedIn
+            </a>
+          </nav>
+          <nav className="footer-link-with-icon">
+            <Twitter />
+            <a href="https://twitter.com/TibzDankan" className="footer-link">
+              Twitter
+            </a>
+          </nav>
+          <nav className="footer-link-with-icon">
+            <Github />
+            <a href="https://github.com/Tibz-Dankan" className="footer-link">
+              Github
+            </a>
+          </nav>
+        </div>
+      </footer>
     </div>
   );
 };
