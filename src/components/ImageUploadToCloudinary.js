0193d64adbed6ded9384fe7a84b1becaaf502af3
Tibz-Dankan <tibesigwadankan@gmail.com>
2022-03-14 19:31:13 +0300

    A function to upload the image url to the backend is added and react spinner is now is conditionally rendered

--- src/components/ImageUploadToCloudinary.js
+++ src/components/ImageUploadToCloudinary.js
@@ -1,18 +1,25 @@
 import { useState } from "react";
 import axios from "axios";
 import "./../css/ImageUploadToCloudinary.css";
+import axiosApiUnAuthorized from "./axiosUnAuthorized.js";
+import { BeatLoader } from "react-spinners";
 
 const ImageUploadToCloudinary = () => {
   const [imageSelected, setImageSelected] = useState("");
   const [imageUrl, setImageUrl] = useState("");
   const [imageCategory, setImageCategory] = useState("");
+  const [imageUploadMsg, setImageUploadMsg] = useState("");
+  const [showBeatLoader, setShowBeatLoader] = useState(false);
 
+  // UploadImage function uploading the image to cloudinary
   const uploadImage = async (e) => {
     const formData = new FormData();
     formData.append("file", imageSelected);
     formData.append("upload_preset", "rxckqye9");
     try {
       e.preventDefault();
+      setShowBeatLoader(true);
+      setImageUploadMsg("Uploading image...");
       const response = await axios.post(
         `https://api.cloudinary.com/v1_1/dlmv4ot9h/image/upload`,
         formData
@@ -20,39 +27,69 @@ const ImageUploadToCloudinary = () => {
       console.log(response);
       if (response.status === 200) {
         setImageUrl(response.data.secure_url);
+        setShowBeatLoader(false);
+        uploadImageUrlToBackend();
         console.log(imageUrl);
       }
     } catch (error) {
       console.log(error);
+      setShowBeatLoader(false);
     }
   };
-  // react spinner when uploading an image
+
+  // https://res.cloudinary.com/dlmv4ot9h/image/upload/v1647263806/xwi9r12edlnqlov9cewh.jpg
+
   // function to store url in the database
+  const uploadImageUrlToBackend = async () => {
+    try {
+      setShowBeatLoader(true);
+      setImageUploadMsg("Uploading url...");
+      const response = await axiosApiUnAuthorized.post(
+        `/api/upload-website-image-url`,
+        {
+          imageUrl: imageUrl,
+          imageCategory: imageCategory,
+        }
+      );
+      console.log(response);
+      if (response.status === 200) {
+        setShowBeatLoader(false);
+        setImageUploadMsg("Uploaded successfully");
+        setImageSelected("");
+      }
+    } catch (error) {
+      setShowBeatLoader(false);
+      setImageUploadMsg("");
+      console.log(error);
+    }
+  };
 
-  // load the image on loading the page
-
-  // checking for a file extension to ensure only images are selected
-  // store the image category in the local storage upon making request to cloudinary api
-  //send image category along with image url from cloudinary for storage in the in the database
-  // if the above request is successful then remove the image category from the local storage
-  // for case of a profile pic, it should be stored along with the user id
   return (
     <div
       className="upload-image-wrapper"
       style={{
-        backgroundImage: `url("https://res.cloudinary.com/dlmv4ot9h/image/upload/v1641992849/pn2aozlq0zulc2yz25ho.jpg")`,
+        // backgroundImage: `url("https://res.cloudinary.com/dlmv4ot9h/image/upload/v1641992849/pn2aozlq0zulc2yz25ho.jpg")`,
+        // backgroundImage: `url(https://res.cloudinary.com/dlmv4ot9h/image/upload/v1647263806/xwi9r12edlnqlov9cewh.jpg)`,
+        backgroundImage: `url(https://res.cloudinary.com/dlmv4ot9h/image/upload/v1647268251/zphcwzcjgn5h8gbhpxb0.jpg)`,
         width: "100%",
-        height: "600px",
+        height: "100%",
         backgroundSize: "cover",
         filter: "opacity(0.9)",
       }}
     >
+      {showBeatLoader ? (
+        <div className="beat-loader">
+          <BeatLoader color={"hsl(180, 100%, 60%)"} />
+          <p>{imageUploadMsg}</p>
+        </div>
+      ) : null}
       <form onSubmit={uploadImage} className="image-upload-form">
         <input
           type="text"
           className="image-category"
           value={imageCategory}
           onChange={(e) => setImageCategory(e.target.value)}
+          placeholder="Image Category"
           required
         />
         <input
